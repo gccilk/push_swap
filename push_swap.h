628f5554019f1785b5aacc78e4541056fda7927f
Ilayda Kaptanoglu <ilkaptan@k1m20s01.42istanbul.com.tr>
2026-09-19 13:49:30 +0300

    operations completed

---
+++ push_swap.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilkaptan <ilkaptan@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/19 13:34:53 by ilkaptan          #+#    #+#             */
+/*   Updated: 2026/09/19 13:42:34 by ilkaptan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "src/stack/stack.h"
+# include "src/operations/operations.h"
+
+#endif
